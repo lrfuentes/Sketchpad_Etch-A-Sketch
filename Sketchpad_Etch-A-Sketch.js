7a109bc120213c0cb4f781201f6af3cb49263a86
Lewis <lewisrodriguezfuentes@gmail.com>
2025-06-17 17:35:24 -0500

    adding trail with js

--- Sketchpad_Etch-A-Sketch.js
+++ Sketchpad_Etch-A-Sketch.js
@@ -2,7 +2,10 @@ let container = document.getElementById('container');
 
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
-        container.appendChild(document.createElement('div'));
+        let div = document.createElement('div');
+        div.addEventListener('mouseover', event => {
+            event.target.classList.add('trail');
+        });
+        container.appendChild(div);
     }
-    
 }
