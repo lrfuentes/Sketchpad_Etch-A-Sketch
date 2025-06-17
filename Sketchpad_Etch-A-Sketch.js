ece9973e0e1a99678bbf5112eb20f7820bb84b50
Lewis <lewisrodriguezfuentes@gmail.com>
2025-06-17 18:01:30 -0500

    adding button and new size

--- Sketchpad_Etch-A-Sketch.js
+++ Sketchpad_Etch-A-Sketch.js
@@ -1,11 +1,25 @@
-let container = document.getElementById('container');
+const container = document.getElementById('container');
+const button = document.querySelector('button');
+createGrid();
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let div = document.createElement('div');
-        div.addEventListener('mouseover', event => {
-            event.target.classList.add('trail');
-        });
-        container.appendChild(div);
+function createGrid(squaresBySide = 16){
+    let removeDivs = document.querySelectorAll('.innerDiv');
+    removeDivs.forEach(item => {
+        item.remove();
+    });
+    for (let i = 0; i < squaresBySide; i++) {
+        for (let j = 0; j < squaresBySide; j++) {
+            let div = document.createElement('div');
+            div.classList.add('innerDiv');
+            div.addEventListener('mouseover', event => {
+                event.target.classList.add('trail');
+            });
+            container.appendChild(div);
+        }
     }
 }
+
+button.addEventListener('click', event => {
+    let result = prompt('ENTER NUMBER OF SQUARES BY SIDE');
+    createGrid(parseInt(result));
+});
