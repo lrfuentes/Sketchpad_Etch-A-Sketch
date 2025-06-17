6d0c8e08fb9ec61aea18a19d5f93c0bfb9a0a0e8
Lewis <lewisrodriguezfuentes@gmail.com>
2025-06-17 10:12:11 -0500

    adding 16*16 divs into div#container

---
+++ Sketchpad_Etch-A-Sketch.js
@@ -0,0 +1,8 @@
+let container = document.getElementById('container');
+
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        container.appendChild(document.createElement('div'));
+    }
+    
+}
